27fae1e098d93eec94670faf4dfe0b945ffaf5d3
lizzielong <lizzielong333@gmail.com>
2025-09-03 13:33:22 -0400

    Added functions "add_student" and "remove_student". I included a text name at the bottom and ran the functions and they are working correctly.

--- agile/sprint1.py
+++ agile/sprint1.py
@@ -1,2 +1,20 @@
 if __name__ == "__main__":
     print("Sprint 1 – starting point (Add/Remove student, Enroll in course).")
+
+students = []
+
+def add_student(student_name):
+    students.append(student_name)
+    print(f"✅ Student {student_name} ")
+
+def remove_student(student_name):
+    if student_name in students:
+        students.remove(student_name)
+        print(f"❌ Student {student_name} ")
+    else:
+        print(f"⚠️ {student_name} is not in this class")
+
+# Run the code only if this file is run directly (not when imported)
+if __name__ == "__main__":
+    add_student("Lizzie")
+    remove_student("Lizzie")
